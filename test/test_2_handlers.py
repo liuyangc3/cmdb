3db2bdee462aec46fefe673ee909497b19635c56
liuyang <liuyangc3@126.com>
2015-10-07 17:22:35 +0800

    增加了 handler  ProjectHandler  get  接口 增加了 handler  ProjectHandler  put  接口 验证service的功能 增加了 orm  Service/Project 的 list 函数
    以及相应的测试用例

--- test/test_2_handlers.py
+++ test/test_2_handlers.py
@@ -21,12 +21,12 @@ class Application(web.Application):
         super(Application, self).__init__(router, **settings)
 
 
-class TestServiceRegexpHanlder(AsyncHTTPTestCase):
+class TestServiceHanlder(AsyncHTTPTestCase):
     def get_app(self):
         return Application()
 
     def setUp(self):
-        super(TestServiceRegexpHanlder, self).setUp()
+        super(TestServiceHanlder, self).setUp()
         self.service_id = '1.1.1.1:8080'
         self.future = Future()
         self.service = Service(io_loop=self.io_loop)
@@ -54,9 +54,9 @@ class TestServiceRegexpHanlder(AsyncHTTPTestCase):
 
     @gen_test(timeout=5)
     def test_11_post_request_not_default_port(self):
-        """ POST /api/v1/service/service_id """
+        # 添加非默认端口的服务
         req = self.form_data_request(
-            self.get_url('/api/v1/service/10.10.10.1:9874'),
+            self.get_url('/api/v1/service/1.1.1.1:9874'),
             method="POST"
         )
         try:
@@ -173,84 +173,135 @@ class TestProjectHandlers(AsyncHTTPTestCase):
     def get_app(self):
         return Application()
 
-    def setUpClass(self):
-        super(TestProjectHandlers, self).setUp()
-        self.project_id = '测试项目'
-        self.project_id_es = url_escape(self.project_id)
-        self.project = Project()
+    @classmethod
+    def setUpClass(cls):
+        cls.project_id = '测试项目'
+        cls.project_id_escape = url_escape(cls.project_id)
 
     def get_new_ioloop(self):
         return IOLoop.instance()
 
-    @gen_test(timeout=3)
-    def tearDown(self):
-        yield self.project.del_doc(self.project_id)
+    def setUp(self):
+        super(TestProjectHandlers, self).setUp()
+        self.service = Service(io_loop=self.io_loop)
+        self.project = Project(io_loop=self.io_loop)
+
+    def form_data_request(self, url, method="POST", **kwargs):
+        boundary = '----WebKitFormBoundary_MyTestBoundary_'
+        body = ''
+        for k, v in kwargs.items():
+            body += '--{0}\r\nContent-Disposition: form-data; name="{1}"\r\n\r\n{2}\r\n'.format(boundary, k, v)
+        body += '--{0}--\r\n'.format(boundary)
+        req = HTTPRequest(
+            url,
+            method=method,
+            headers={'Content-Type': 'multipart/form-data; boundary={0}'.format(boundary)},
+            body=body
+        )
+        return req
 
     @gen_test(timeout=3)
-    def test_project_get(self):
-        yield self.project.add_project(self.project_id, {"type": "project"})
+    def test_11_project_post(self):
         response = yield self.http_client.fetch(
-            self.get_url('/api/v1/project/{0}'.format(self.project_id_es)),
+            self.get_url('/api/v1/project/{0}'.format(self.project_id_escape)),
+            method="POST",
+            body=""
+        )
+        r = json_decode(response.body)
+        self.assertEqual(r['ok'], True)
+
+    @gen_test(timeout=3)
+    def test_12_project_post_again(self):
+        try:
+            yield self.http_client.fetch(
+                self.get_url('/api/v1/project/{0}'.format(self.project_id_escape)),
+                method="POST",
+                body=""
+            )
+        except HTTPError as e:
+            self.assertEqual(e.message, 'HTTP 500: Project exist')
+
+    @gen_test(timeout=3)
+    def test_21_projects_get(self):
+        response = yield self.http_client.fetch(
+            self.get_url('/api/v1/project/list'),
             method="GET"
         )
         r = json_decode(response.body)
-        self.assertEqual(r, [self.project_id])
+        for project in r['rows']:
+            self.assertEqual(project['key'], self.project_id)
 
     @gen_test(timeout=3)
-    def test_project_post(self):
+    def test_22_project_get(self):
         response = yield self.http_client.fetch(
-            self.get_url('/api/v1/project/{0}'.format(self.project_id_es)),
-            method="POST",
-            body=""
+            self.get_url('/api/v1/project/{0}'.format(self.project_id_escape)),
+            method="GET"
         )
         r = json_decode(response.body)
-        self.assertEqual(r['ok'], True)
-
-        response = yield self.http_client.fetch(
-            self.get_url('/api/v1/project/{0}'.format(self.project_id_es)),
-            method="POST",
-            body=""
-        )
-        r = json_decode(response.body)
-        self.assertEqual(r['ok'], False)
-        self.assertEqual(r['msg'], 'Project exist')
+        self.assertEqual(r['_id'], self.project_id)
 
     @gen_test(timeout=5)
-    def test_project_put(self):
-        response = yield self.http_client.fetch(
-            self.get_url('/api/v1/project/{0}'.format(self.project_id_es)),
+    def test_31_project_put(self):
+        req = self.form_data_request(
+            self.get_url('/api/v1/project/{0}'.format(self.project_id_escape)),
             method="PUT",
-            body=""
+            field="whatever"
         )
+        response = yield self.http_client.fetch(req)
         r = json_decode(response.body)
-        self.assertEqual(r['ok'], False)
-        self.assertEqual(r['msg'], 'Project Not Found')
+        self.assertEqual(r['ok'], True)
+        doc = yield self.project.get_doc(self.project_id_escape)
+        self.assertEqual(doc['field'], 'whatever')
 
-        yield self.project.add_project(self.project_id, {"type": "project"})
-        response = yield self.http_client.fetch(
-            self.get_url('/api/v1/project/{0}'.format(self.project_id_es)),
-            method="PUT",
-            body=""
-        )
-        r = json_decode(response.body)
-        self.assertEqual(r['ok'], False)
-        self.assertEqual(r['msg'], 'Request body is empty')
+    @gen_test(timeout=5)
+    def test_32_project_put_no_body(self):
+        try:
+            yield self.http_client.fetch(
+                self.get_url('/api/v1/project/{0}'.format(self.project_id_escape)),
+                method="PUT",
+                body=""
+            )
+        except HTTPError as e:
+            self.assertEqual(e.message, 'HTTP 500: Request body is empty')
 
-        response = yield self.http_client.fetch(
-            self.get_url('/api/v1/project/{0}'.format(self.project_id_es)),
+    @gen_test(timeout=5)
+    def test_33_project_put_add_service(self):
+        # add 2 test service
+        request_body = {"type": "service"}
+        service_id1 = "1.1.1.1:8080"
+        service_id2 = "2.2.2.2:9090"
+        yield self.service.add_service(service_id1, request_body)
+        yield self.service.add_service(service_id2, request_body)
+
+        req = self.form_data_request(
+            self.get_url('/api/v1/project/{0}'.format(self.project_id_escape)),
             method="PUT",
-            body=json_encode({"services": '["111:8080", "222:8081"]'})
+            services='["{0}","{1}"]'.format(service_id1, service_id2)
         )
+        response = yield self.http_client.fetch(req)
         r = json_decode(response.body)
         self.assertEqual(r['ok'], True)
 
-        yield self.http_client.fetch(
-            self.get_url('/api/v1/project/{0}'.format(self.project_id_es)),
+    @gen_test(timeout=5)
+    def test_34_project_put_add_service_not_exist(self):
+        req = self.form_data_request(
+            self.get_url('/api/v1/project/{0}'.format(self.project_id_escape)),
             method="PUT",
-            body=json_encode({"services": '["333:8082"]'})
+            services='["1.2.3.4:1234"]'
         )
-        response = yield self.project.get_doc(self.project_id)
-        self.assertEqual(response['services'], ["222:8081", "111:8080", "333:8082"])
+        try:
+            yield self.http_client.fetch(req)
+        except HTTPError as e:
+            self.assertEqual(e.message, 'HTTP 500: Service 1.2.3.4:1234 not exist')
+
+    @gen_test(timeout=5)
+    def test_35_del_33_service(self):
+        yield self.service.del_doc("1.1.1.1:8080")
+        yield self.service.del_doc("2.2.2.2:9090")
+
+    @gen_test(timeout=3)
+    def test_9_tearDown(self):
+        yield self.project.del_doc(self.project_id)
 
 
 def setup_fetch(mock_fetch, status_code, body=None):
