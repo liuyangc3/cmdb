6f2a5c97b85e543e5c085decba5a9e6279f1bffe
liuyang <liuyangc3@126.com>
2015-10-22 12:25:24 +0800

    set up

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="cmdb",
+    version="0.1",
+    description="cmdb backend",
+    author="liuyang",
+    install_requires=[
+        "tornado>=4.0",
+    ]
+)
